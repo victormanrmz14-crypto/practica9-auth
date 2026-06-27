022efec73d6732646ed802380d4c2e444ffd013a
Victor Manuel Ramirez <victorman.rmz14@gmail.com>
2026-06-26 20:06:26 -0600

    creacion de vista para vizalizar el perfil del usuario y modificacion de la ruta web.php para agregar la ruta /perfil

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,17 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Rutas que ya vienen con Breeze
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Ruta personalizada de la práctica
+    Route::get('/perfil', function () {
+        return view('perfil', [
+            'user' => auth()->user()
+        ]);
+    })->name('perfil');
 });
 
 require __DIR__.'/auth.php';
